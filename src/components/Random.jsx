57625839cef22b5cb81c6f9d7a29ce1baca172d1
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-03-23 17:04:21 +0530

    class component lifecycle methods changes

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -4,10 +4,32 @@
 import React from 'react';
 
 export class Random extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {course: "MERN"};
+    }
+
+    componentDidMount() {
+        console.log("course value in state",this.state.course);
+        setTimeout(() => {
+            this.setState({course: "MEAN"})
+        }, 1000);
+        console.log("course value in state after updation",this.state.course);
+
+    }
+
+    changeCourse = ()=>{
+        this.setState({course:"MEAN"});
+    }
+
     render() {
         return(
             <div>
                 <p>Inside Random Component</p>
+                <p>Random component state course value {this.state.course}</p>
+                <button style={{backgroundColor:'black',color:'white'}}
+                onClick={this.changeCourse}>Click here to update state value</button>
             </div>
         )
     }
